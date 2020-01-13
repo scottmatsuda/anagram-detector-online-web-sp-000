2d1c6c473a4b58bc7ba2ae44a168b9f67767313d
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-13 20:24:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    match = []
+      array.each do |word|
+        if @word.length == word.length && @word.include?(word)
+          match << word
+        end
+      end
+    match
+  end
+end
